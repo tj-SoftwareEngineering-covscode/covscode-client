a9c6d04e5282868766d0ed58c324ab6f7449ac2e
CChris152 <1650306859@qq.com>
2024-12-02 01:24:28 +0800

    fix 修bug

--- out/constant.js
+++ out/constant.js
@@ -1,12 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.QuitOptionName = exports.JoinOptionName = exports.CreateOptionName = exports.StartOption = exports.CurrentStatus = exports.currentStatus = void 0;
-exports.currentStatus = CurrentStatus.Off;
+exports.QuitOptionName = exports.JoinOptionName = exports.CreateOptionName = exports.StartOption = exports.currentStatus = exports.CurrentStatus = void 0;
 var CurrentStatus;
 (function (CurrentStatus) {
     CurrentStatus[CurrentStatus["On"] = 0] = "On";
     CurrentStatus[CurrentStatus["Off"] = 1] = "Off";
 })(CurrentStatus || (exports.CurrentStatus = CurrentStatus = {}));
+//当前插件的运行状态
+exports.currentStatus = CurrentStatus.Off;
 var StartOption;
 (function (StartOption) {
     StartOption[StartOption["Create"] = 0] = "Create";
--- src/constant.ts
+++ src/constant.ts
@@ -1,9 +1,10 @@
-export let currentStatus:CurrentStatus=CurrentStatus.Off;
-
 export enum CurrentStatus{
   On,
   Off
 }
+
+//当前插件的运行状态
+export let currentStatus:CurrentStatus=CurrentStatus.Off;
 
 export enum StartOption {
   Create,
